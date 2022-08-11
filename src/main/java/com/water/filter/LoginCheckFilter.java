6a80371aeb0f18b9d20779e902eaf6612a76c879
Yaml <993620479@qq.com>
2022-08-11 18:43:15 +0800

    加入Swagger

--- src/main/java/com/water/filter/LoginCheckFilter.java
+++ src/main/java/com/water/filter/LoginCheckFilter.java
@@ -40,7 +40,11 @@ public class LoginCheckFilter implements Filter {
                 "/front/**",
                 "/common/**",
                 "/user/sendMsg",//移动端发送短信
-                "/user/login" //移动端登录
+                "/user/login", //移动端登录
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
         boolean check = check(urls, requestURI);
 
